d1db6bd6ea5ab745694f3dc526cde6386ef20f16
lgvidal <lgvidal@gmail.com>
2016-04-05 02:04:12 -0300

    Algunos tests (incluí los del enunciado).
    - Cambié el test del enunciado de KNN, con 2 no está garantizado que de
    (moda puede tomar el otro, misma cantidad de ocurrencias).

--- src/Tests-alu.hs
+++ src/Tests-alu.hs
@@ -15,7 +15,12 @@ allTests = test [
     "e4repPromedio" ~: testsE4RepPromedio,
     "e5frecTokens" ~: testsE5FrecTokens,
     "e6normExtractor" ~: testsE6NormExtractor,
-    "e7extraerFeatures" ~: testsE7ExtraerFeatures
+    "e7extraerFeatures" ~: testsE7ExtraerFeatures,
+    "e8aDistanciaEuclidiana" ~: testsE8aDistanciaEuclidiana,
+    "e8bDistanciaCoseno" ~: testsE8bDistanciaCoseno,
+    "e9knn" ~: testsE9KNN,
+    "e10separarDatos" ~: testsE10SepararDatos,
+    "e11accuracy" ~: testsE11Accuracy
     ]
 
 testsE1Split = test [
@@ -59,6 +64,42 @@ testsE6NormExtractor = test [
     normalizarExtractor ["1", "3 3 3", "5 5 5 5 5"] repeticionesPromedio "2 2 3 3 3" ~?= 0.5
     ]
     
-testsE7ExtraerFeatures = test [ -- El ejemplo del enunciado
-    extraerFeatures [longitudPromedioPalabras, repeticionesPromedio] ["b=a", "a = 2; a = 4", "C:/DOS C:/DOS/RUN RUN/DOS/RUN"] ~?= [[0.33333334,0.6666667],[0.12962963,1.0],[1.0,0.6666667]]
+testsE7ExtraerFeatures = test [
+    extraerFeatures [longitudPromedioPalabras, repeticionesPromedio] ["b=a", "a = 2; a = 4", "C:/DOS C:/DOS/RUN RUN/DOS/RUN"] ~?= [[0.33333334,0.6666667],[0.12962963,1.0],[1.0,0.6666667]] -- Ejemplo del enunciado
     ]
+    
+testsE8aDistanciaEuclidiana = test [
+    distEuclidiana [0,0] [0,0] ~?= 0.0,
+    distEuclidiana [1,0] [0,0] ~?= 1.0,
+    distEuclidiana [0,1] [0,0] ~?= 1.0,
+    distEuclidiana [3,2] [0,6] ~?= 5.0,
+    distEuclidiana [1.0,0.75,0.8125] [0.75,1.0,0.5] ~?= 0.47186464 -- Ejemplo del enunciado
+    ]
+
+testsE8bDistanciaCoseno = test [
+    distCoseno [1,1] [5,5] ~?= 1.0,
+    distCoseno [0,3,4] [0,-3,-4] ~?= -1.0 -- Ejemplo del enunciado
+    ]
+
+testsE9KNN = test [
+        (knn 3 [[0,1],[0,2],[2,1],[1,1],[2,3]] ["i","i","f","f","i"] distEuclidiana) [1,1] ~?= "f", -- Ejemplo del enunciado, con k=3. Con k=2 queda una "f" ([1,1]) y una "i" ([0,1] está tan cerca como [2,1]), ambas son moda.
+        (knn 1 [[0], [1], [2], [5], [6], [7], [8]] ["a", "a", "a", "b", "a", "a", "a"] distEuclidiana) [4] ~?= "b",
+        (knn 4 [[0], [1], [2], [3], [4], [5], [6]] ["a", "a", "a", "b", "b", "c", "d"] distEuclidiana) [10] ~?= "b",
+        -- DistCoseno es menor cuanto más lejos estás (-1).
+        (knn 1 [[1,1], [1,-1], [-1,-1], [-1, 1]] ["cuad3", "cuad4", "cuad1", "cuad2"] distCoseno) [3,7] ~?= "cuad1",
+        (knn 1 [[1,1], [1,-1], [-1,-1], [-1, 1]] ["cuad3", "cuad4", "cuad1", "cuad2"] distCoseno) [10,-3] ~?= "cuad2",
+        (knn 1 [[1,1], [1,-1], [-1,-1], [-1, 1]] ["cuad3", "cuad4", "cuad1", "cuad2"] distCoseno) [-3,-7] ~?= "cuad3",
+        (knn 1 [[1,1], [1,-1], [-1,-1], [-1, 1]] ["cuad3", "cuad4", "cuad1", "cuad2"] distCoseno) [-35,11] ~?= "cuad4"
+    ]
+
+testsE10SepararDatos = test [
+        separarDatos [[1,1],[2,2],[3,3],[4,4],[5,5],[6,6],[7,7]] ["1","2","3","4","5","6","7"] 3 2 ~?= ([[1.0,1.0],[2.0,2.0],[5.0,5.0],[6.0,6.0]],[[3.0,3.0],[4.0,4.0]],["1","2","5","6"],["3","4"]), -- Ejemplo del enunciado
+        separarDatos [[1], [2], [3]] ["1", "2", "3"] 3 1 ~?= ([[2],[3]], [[1]], ["2", "3"], ["1"])
+    ]
+
+testsE11Accuracy = test [
+        accuracy [""] [""] ~?= 1.0,
+        accuracy ["1", "2"] ["2", "2"] ~?= 0.5,
+        accuracy ["a", "b", "c", "d", "e"] ["e", "d", "c", "b", "a"] ~?= 0.2
+    ]
+
